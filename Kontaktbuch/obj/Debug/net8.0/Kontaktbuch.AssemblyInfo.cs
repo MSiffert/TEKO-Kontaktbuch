51bb062ea6c5517dd687a043a37bb68a6cad54d5
Marco Siffert <marco.siffert@gmail.com>
2024-03-17 19:52:19 +0100

    initial with gitignore

--- Kontaktbuch/obj/Debug/net8.0/Kontaktbuch.AssemblyInfo.cs
+++ Kontaktbuch/obj/Debug/net8.0/Kontaktbuch.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Kontaktbuch")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c777c1fc3042616195f3db0867a45526594c7e90")]
 [assembly: System.Reflection.AssemblyProductAttribute("Kontaktbuch")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Kontaktbuch")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
